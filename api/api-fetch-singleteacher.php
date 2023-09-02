7372cca8a09e39552c3e8f0fefe991b25d99b97a
Rakib Ahsan <rakibzubayar@gmail.com>
2023-09-03 00:19:49 +0600

    api

---
+++ api/api-fetch-singleteacher.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+include "config.php";
+
+$id = $_GET['id'];
+
+$sql = "SELECT * FROM teacher WHERE tea_id = {$id}";
+
+$result = mysqli_query($conn, $sql) or die("SQL Query Failed.");
+
+if (mysqli_num_rows($result) > 0) {
+    $output = mysqli_fetch_assoc($result);
+    echo json_encode($output);
+} else {
+    echo json_encode(array('message' => 'No Record Found.', 'status' => false));
+}
+?>
---
+++ api/config.php
@@ -0,0 +1,5 @@
+<?php 
+
+$conn = mysqli_connect("localhost","root","","10ms") or die("Connection Failed");
+
+ ?>
