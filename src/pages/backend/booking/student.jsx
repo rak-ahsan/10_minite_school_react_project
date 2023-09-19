7ee95fd8d0185393f2e3013377d99ca796ba6227
rak-ahsan <rakibzubayar@gmail.com>
2023-09-19 10:39:51 +0600

    comment

--- src/pages/backend/booking/student.jsx
+++ src/pages/backend/booking/student.jsx
@@ -1,98 +1,77 @@
-import React from 'react'
-import Header from '../../../component/heback'
+import React, { useEffect, useState } from 'react';
+import Header from '../../../component/heback';
+import Frominp from '../../../component/frominp';
+import BtnSubmit from '../../../component/bntsubmit';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-const student = () => {
+const Student = () => {
+    const [comment, setComment] = useState({
+        comment: ''
+    });
+
+    const [comments, setComments] = useState([]);
+
+    const input = (e) => {
+        e.persist();
+        setComment({ ...comment, [e.target.name]: e.target.value });
+    };
+
+    const reset = () => {
+        setComment({
+            comment: "",
+        });
+    }
+    const save = (e) => {
+        e.preventDefault();
+
+        const data = {
+            comment: comment.comment
+        };
+
+        axios.post(`http://localhost/ReactProject/api/api-insert-comment.php`, data).then(res => {
+            axios.get(`http://localhost/ReactProject/api/api-fetch-comment.php`).then(res => {
+                setComments(res.data);
+            });
+
+            reset()
+        });
+    };
+
+    useEffect(() => {
+        axios.get(`http://localhost/ReactProject/api/api-fetch-comment.php`).then(res => {
+            setComments(res.data);
+        });
+    }, []);
+
     return (
         <>
-        <Header />
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">Card Title</h5>
-                            <p class="card-text">
-                                <iframe width="300" height="250" src="https://www.youtube.com/embed/8PtKzVbdS14" title="৪টি ধাপে সম্পূর্ণ HSC 25 সিলেবাস প্রস্তুতি | HSC 2025 Online Batch" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">Card Title</h5>
-                            <p class="card-text"><iframe width="300" height="250" src="https://www.youtube.com/embed/pJ5ShYoCiFQ" title="HSC লাইফে যে ২০ টি ভুল করা যাবে না | HSC 25" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">Card Title</h5>
-                            <p class="card-text"><iframe width="300" height="250" src="https://www.youtube.com/embed/PpKCjxQsvBM?list=PLh2EsjwYJx7td4koLEze5b_emtb0JY1Nb" title="HSC 2025 Guideline | এইচ এস সি ২০২৫ কিভাবে চিন্তা করবে । আফ্রী হাসান । Fahad&#39;s Tutorial" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div><div class="container">
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Card Title</h5>
-                                <p class="card-text">
-                                    <iframe width="300" height="250" src="https://www.youtube.com/embed/8PtKzVbdS14" title="৪টি ধাপে সম্পূর্ণ HSC 25 সিলেবাস প্রস্তুতি | HSC 2025 Online Batch" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Card Title</h5>
-                                <p class="card-text"><iframe width="300" height="250" src="https://www.youtube.com/embed/pJ5ShYoCiFQ" title="HSC লাইফে যে ২০ টি ভুল করা যাবে না | HSC 25" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Card Title</h5>
-                                <p class="card-text"><iframe width="300" height="250" src="https://www.youtube.com/embed/PpKCjxQsvBM?list=PLh2EsjwYJx7td4koLEze5b_emtb0JY1Nb" title="HSC 2025 Guideline | এইচ এস সি ২০২৫ কিভাবে চিন্তা করবে । আফ্রী হাসান । Fahad&#39;s Tutorial" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Card Title</h5>
-                                <p class="card-text">
-                                    <iframe width="300" height="250" src="https://www.youtube.com/embed/8PtKzVbdS14" title="৪টি ধাপে সম্পূর্ণ HSC 25 সিলেবাস প্রস্তুতি | HSC 2025 Online Batch" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
-                            </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Card Title</h5>
-                                <p class="card-text"><iframe width="300" height="250" src="https://www.youtube.com/embed/pJ5ShYoCiFQ" title="HSC লাইফে যে ২০ টি ভুল করা যাবে না | HSC 25" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
-                            </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Card Title</h5>
-                                <p class="card-text"><iframe width="300" height="250" src="https://www.youtube.com/embed/PpKCjxQsvBM?list=PLh2EsjwYJx7td4koLEze5b_emtb0JY1Nb" title="HSC 2025 Guideline | এইচ এস সি ২০২৫ কিভাবে চিন্তা করবে । আফ্রী হাসান । Fahad&#39;s Tutorial" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></p>
-                            </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-            </>
-    )
-}
+            <Header />
+            <div className="container">
+            <form onSubmit={save}>
+                <Frominp
+                    title="Post Your Comment"
+                    placeholder="comment"
+                    type="text"
+                    name="comment"
+                    className="col-md-4 mt-5"
+                    onChange={input}
+                    value={comment.comment}
+                />
+                <BtnSubmit text="ADD Comment" className="btn-success" type="submit" />
+            </form>
 
-export default student
+            {comments.length > 0 && (
+                comments.map((item, index) => (
+                    <tr key={index}>
+                        <td>{item.comment}</td>
+                    </tr>
+                ))
+            )}
+            </div>
+            
+        </>
+    );
+};
+
+export default Student;
