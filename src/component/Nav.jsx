927be8096e87773de15579698ee40a63f8de850c
Rakib Ahsan <rakibzubayar@gmail.com>
2024-05-28 22:32:43 +0600

    t

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,8 +1,5 @@
-import logo from "../assets/logo.png"
 import Home from "../component/Home.jsx"
 import Footer from "./footer"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faPhone } from '@fortawesome/free-solid-svg-icons'
 import "../css/nav.css"
 import Head from "../component/he"
 function nav() {
